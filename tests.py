7ce93a4f8d6d708660756828cec9bea31b85bb3c
Guillaume Pellerin <yomguy@parisson.com>
2014-12-13 00:48:44 +0100

    update libcodegen, fix test against 1.42

--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ import simplejson
 import unittest
 
 class EchoprintTest(unittest.TestCase):
+    """Test EchoPrint code"""
+
     def test_codegen(self):
         d = echoprint.codegen(simplejson.load(open('test_data.json')))
-        self.assertEqual(d['code'], 'eJydz7sNAzAIRdGV-GPGiQHvP0KcypVdpDnNFUIPANDhAdWL963AC7T_YbrSnjJMocwJo7Qy5FAELdcqtpBt6Ezu38I0r4MH7e-3iroTDooJAZO7LO2AWeszaBXyXt_anXgQlcBr_QIy12Qh')
-        self.assertEqual(d['version'], '4.11')
+        self.assertEqual(d['code'], 'eJyd0juuQyEMRdEp2cbYZjjgz_yHEKrHy1VCkWYVW0I6CAAAFX6G4sb9LMMNtBtMN3q_cV_V2g2U32n0FUusWpTsn0h1NukQooQjImzVPwiSn-0PlsIm1pe33K9S06QOOgjw0Q7YNMEEW7U1vRvGhMzBqmpTi2B23KNkjBg83YEOblrz0Q5WwV5jmWUsXct65YES9_T3dsC-ryO8mnwCPfZ2qsC2_1F6H2QH7jzw0Q4vGHW94A==')
+        self.assertEqual(d['version'], 4.12)
 
